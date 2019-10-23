9ebe53fe35c23612fca0dcf98c403d01b6168fe8
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-10-23 15:08:37 -0700

    Persistence added for group members

--- FoodyFriends/FoodyFriends/Model Controllers/GroupMembersModelController.swift
+++ FoodyFriends/FoodyFriends/Model Controllers/GroupMembersModelController.swift
@@ -12,15 +12,55 @@ class GroupMembersModelController {
     
     var groupMembers = [Member]()
     
+    init() {
+        loadFromPersistentStore()
+    }
+    
     func addNewMember(_ name: String) {
         let newMember = Member(name)
         groupMembers.append(newMember)
+        saveToPersistentStore()
     }
     
     func deleteMember(_ member: Member) {
         guard let memberIndex = groupMembers.firstIndex(of: member) else { return }
         groupMembers.remove(at: memberIndex)
+        saveToPersistentStore()
     }
     
+    // MARK: - Persistence
     
+    private var groupMembersURL: URL? {
+        let fm = FileManager.default
+        guard let dir = fm.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        let filename = "GroupMembers.plist"
+        return dir.appendingPathComponent(filename)
+    }
+        
+    private func saveToPersistentStore() {
+        guard let url = groupMembersURL else { return }
+        
+        do {
+            let encoder = PropertyListEncoder()
+            let groupMembersData = try encoder.encode(groupMembers)
+            try groupMembersData.write(to: url)
+        } catch {
+            print("Error saving places to eat data: \(error)")
+        }
+    }
+    
+    private func loadFromPersistentStore() {
+        let fm = FileManager.default
+        guard let url = groupMembersURL,
+            fm.fileExists(atPath: url.path) else { return }
+        
+        do {
+            let groupMembersData = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            let decodedGroupMembers = try decoder.decode([Member].self, from: groupMembersData)
+            groupMembers = decodedGroupMembers
+        } catch {
+            print("Error loading places to eat data: \(error)")
+        }
+    }
 }
